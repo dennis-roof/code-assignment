fc5cd81f57adaa3359fb2c418e3b62ee1b4fffb9
Dennis Roof <d.roof@miwebb.com>
2024-04-20 15:30:21 +0200

    Removed nonsense response body and changed http status to 204 for GET /healthz endpoint

--- backend/src/main/kotlin/nl/infowijs/codeassignment/controllers/HealthController.kt
+++ backend/src/main/kotlin/nl/infowijs/codeassignment/controllers/HealthController.kt
@@ -6,7 +6,7 @@ import io.vertx.ext.web.RoutingContext
 class HealthController {
   companion object {
     val healthCheck = Handler<RoutingContext> { req ->
-      req.response().setStatusCode(200).end("Remove this.")
+      req.response().setStatusCode(204).end()
     }
   }
 }
